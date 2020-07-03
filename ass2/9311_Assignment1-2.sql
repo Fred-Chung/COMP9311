b9ac267de04fc7d4c867ac3e8784d8fbd9d9b8cf
XPeng <zhonghq@xiaopeng.com>
2020-07-03 12:49:26 +0800

    debug

--- ass2/9311_Assignment1-2.sql
+++ ass2/9311_Assignment1-2.sql
@@ -42,11 +42,11 @@ create table Places (
 	state       varchar,
 	country    varchar(50) not null,
 	postalcode  integer,
-	gpscoords	LocationValue , ------------------------bug
+	gpscoords	LocationValue unique, ------------------------bug
 	billingAddress  varchar,
 	homeAddress varchar,
 	creater_user integer,
-	primary key (id,creater_user,name)
+	primary key (id)
 );
 
 
@@ -93,7 +93,7 @@ create table Organisers (
 
 create table PageColours (
 	id      serial,
-	owner	ColourValue ,
+	owner	integer,
 	maintext ColourValue,
 	heading ColourValue,
 	headtext	ColourValue,
@@ -119,15 +119,15 @@ create table ContactLists (
 
 -- EVENTS: things that happen and which people attend via tickets
 create table categories(
-    EventInfoID   serial
-                  references EventInfo(id),
-    categories    text varchar(100),
-    primary key (EventinfoId)
+    EventInfoID   serial,
+    categories    varchar(100),
+    primary key (EventInfoID)
 );
 
 
 create table EventInfo (
 	id          serial,
+	pagecolours_id integer,
 	showFee  	boolean not null,
 	showLeft 	boolean	not null,
 	isPrivate	boolean	not null ,
@@ -142,6 +142,7 @@ create table EventInfo (
 
 create table Events (
 	id          serial,
+	RepeatingEvents_id integer,
 	event_info  integer not null,  
 	startDate	date not null,
 	startTime   time not null,
@@ -159,49 +160,43 @@ create domain DayOfWeekType as char(3)
 
 create table RepeatingEvents (
 	id          serial,
-	lowerDate   date not null
-	upperDate   date not null
-	            check (lowerDate > upperDate)
-	event_id     serial
-	            references Events(id),
-    EventinfoId      serial
-                references EventInfo(id),
-	...
+	eventinfo_id integer,
+	lowerDate   date not null,
+	upperDate   date not null check (lowerDate < upperDate),
+	event_id     serial,
+    EventinfoId      serial,
 	primary key (id)
 );
 
 create table DailyEvents (
- 	id          serial
+ 	id          serial,
  	frequency   int not null 
- 	            check(frequency >= 1 and frequency <= 31)
- 	event_id     serial
- 	            references RepeatingEvents(id)
+ 	            check(frequency >= 1 and frequency <= 31),
+ 	event_id     serial,
  	primary key (id)
 );
 
 create table WeeklyEvents (
-    id          serial
+    id          serial,
     frequency   int not null 
- 	            check(frequency >= 1 and frequency <= 4)
+ 	            check(frequency >= 1 and frequency <= 4),
     DayOfWeek   DayOfWeekType not null,
-    event_id    serial
-                references RepeatingEvents(id),
+    event_id    serial,
     primary key (id)
 );
 
 
 create table MonthlyByDayEvents (
-    id          serial
+    id          serial,
     DayOfWeek   DayOfWeekType not null,
     weekInMonth int not null
-                check(weekInMonth >= 1 and weekInMonth) <= 5)
-    event_id    serial
-                references RepeatingEvents(id),
+                check(weekInMonth >= 1 and weekInMonth <= 5),
+    event_id    serial,
     primary key (id)
 );
 
 -- TICKETS: things that let you attend an event
-create domain tK_Type as varchar
+create domain tK_Type as varchar;
 	
 create table TicketTypes (
 	id          serial unique,
@@ -231,8 +226,7 @@ alter table Places add constraint place_user foreign key(creater_user) reference
 
 --user
 alter table Users add constraint user_people foreign key(id) references People(id);
-alter table Users add constraint user_place foreign key(billingAddress) references place(id);
-alter table Users add constraint user_place foreign key(homeAddress) references People(id);
+
 --orgainser
 alter table Organisers add constraint organ_user foreign key (create_user) references Users(id);
 alter table Organisers add constraint organ_pagecolor foreign key (theme) references PageColours(id);
@@ -242,7 +236,7 @@ alter table PageColours add constraint pagecolor_user	foreign key (owner) refere
 alter table ContactLists add constraint	concat_people foreign key (people) references People(id);
 alter table ContactLists add constraint	concat_user foreign key(owner_user) references Users(id);
 ------eventinfo
-alter table EventInfo add constraint	eventinfo_places foreign key (location) references Places(id);
+alter table EventInfo add constraint	eventinfo_places foreign key (location) references Places(gpscoords);
 alter table EventInfo add constraint	eventinfo_organisers	foreign key (event_organiser) references Organisers(id);
 alter table EventInfo add constraint	eventinfo_pagecolours foreign key (pagecolours_id) references pagecolours(id);
 
@@ -267,59 +261,59 @@ alter table SoldTickets add constraint sold_tick   foreign key (ticket_type) ref
 
 
 
--------- input people
-insert into  people(email,givenname) values ('1@mail.','a'),('2@mail.','a1'),('3@mail.','a3'),('4@mail.','a4'),('5@mail.','a5');
+-- -------- input people
+-- insert into  people(email,givenname) values ('1@mail.','a'),('2@mail.','a1'),('3@mail.','a3'),('4@mail.','a4'),('5@mail.','a5');
 
--------- insert user
-insert into users (id,password,billingAddress)
-values (1,'avc','c1'),(2,'avc','c2'),(3,'avc','c3'),(4,'avc','c4');
+-- -------- insert user
+-- insert into users (id,password,billingAddress)
+-- values (1,'avc','c1'),(2,'avc','c2'),(3,'avc','c3'),(4,'avc','c4');
 
--------- insert theme
-insert into PageColours(owner,heading) values(1,'white'),(2,'black'),(3,'light');
+-- -------- insert theme
+-- insert into PageColours(owner) values(1),(2),(3);
 
---------- insert organiser
-insert into Organisers(create_user,name,theme) values(1,'first',1),(1,'second',2),(2,'second',2),(3,'third',3);
+-- --------- insert organiser
+-- insert into Organisers(create_user,name,theme) values(1,'first',1),(1,'second',2),(2,'second',2),(3,'third',3);
 
----------- places
-insert into Places(name,address,country,creater_user) 
-values('home','gaungzhou','china',1) ,
-('home','hangzhou','china',2),
-('home','huizhou','china',3);
+-- ---------- places
+-- insert into Places(name,address,country,creater_user) 
+-- values('home','gaungzhou','china',1) ,
+-- ('home','hangzhou','china',2),
+-- ('home','huizhou','china',3);
 
----------- insert event_info
-insert into EventInfo (showFee,showLeft,isPrivate,title,details,duration,event_organiser)values (True,True,True,'sleep','slllllll','00:40',1),
-(True,True,True,'walk','sllllllasd','10:40',2),
-(True,True,True,'run','slladsfl','02:40',2),
-(True,True,True,'singing','slsdflllll','01:40',3);
+-- ---------- insert event_info
+-- insert into EventInfo (showFee,showLeft,isPrivate,title,details,duration,event_organiser)values (True,True,True,'sleep','slllllll','00:40',1),
+-- (True,True,True,'walk','sllllllasd','10:40',2),
+-- (True,True,True,'run','slladsfl','02:40',2),
+-- (True,True,True,'singing','slsdflllll','01:40',3);
 
----------- insert event
-insert into Events(Event_Info,startDate,startTime)
-values (1,'2020-01-04','07:40'),
-(2,'2020-01-01','07:40'),
-(2,'2020-01-08','07:40'),
-(2,'2020-01-15','07:40'),
-(3,'2020-01-01','07:40'),
-(1,'2020-01-05','07:40'),
-(1,'2020-01-06','07:40'),
-(1,'2020-01-07','07:40'),
-(1,'2020-01-08','07:40'),
-(1,'2020-01-09','07:40');
+-- ---------- insert event
+-- insert into Events(Event_Info,startDate,startTime)
+-- values (1,'2020-01-04','07:40'),
+-- (2,'2020-01-01','07:40'),
+-- (2,'2020-01-08','07:40'),
+-- (2,'2020-01-15','07:40'),
+-- (3,'2020-01-01','07:40'),
+-- (1,'2020-01-05','07:40'),
+-- (1,'2020-01-06','07:40'),
+-- (1,'2020-01-07','07:40'),
+-- (1,'2020-01-08','07:40'),
+-- (1,'2020-01-09','07:40');
 
----------- repeat event
-insert into RepeatingEvents (lowerDate,upperDate,repeat_time)
-values ('2020-01-04','2020-01-09','daily');
+-- ---------- repeat event
+-- insert into RepeatingEvents (lowerDate,upperDate,repeat_time)
+-- values ('2020-01-04','2020-01-09','daily');
 
--------ticket
-insert into TicketTypes (event_id,tick_type,totalnumber,max_per_sale)
-values (1,'Standard',200,2),
-(2,'Standard',100,2),
-(3,'First Class',300,5),
-(4,'Standard',600,20);
+-- -------ticket
+-- insert into TicketTypes (event_id,tick_type,totalnumber,max_per_sale)
+-- values (1,'Standard',200,2),
+-- (2,'Standard',100,2),
+-- (3,'First Class',300,5),
+-- (4,'Standard',600,20);
 
------soldtick
-insert into SoldTickets(event_id,quantity,ticket_type,purchaser,url_for_pur)
-values (1,1,1,5,'http://adfasd'),
-(1,2,1,2,'http://adsfadfasdadsf'),
-(2,1,2,1,'http://adfasdadsf'),
-(4,3,4,2,'http://adfasdads'),
-(3,1,3,4,'http://adfasdadfads');
+-- -----soldtick
+-- insert into SoldTickets(event_id,quantity,ticket_type,purchaser)
+-- values (1,1,1,5),
+-- (1,2,1,2),
+-- (2,1,2,1),
+-- (4,3,4,2),
+-- (3,1,3,4);
